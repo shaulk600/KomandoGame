32881d1644a4ce3c4d596e46386ca6db3f337769
Shaul Kimel <shaullk600@gmail.com>
2025-05-28 17:12:59 +0300

    Posh To Folder Enemies

---
+++ Enemies/EnemyFactory.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using KomandoOOP;
+using KomandoOOP.Commands;
+using KomandoOOP.GameLogic;
+
+namespace KomandoOOP.Enemies
+{
+    internal class EnemyFactory
+    {
+        /// <summary>
+        ///  this Class is Factory
+        /// </summary>
+
+        // variable Singletone
+        private static EnemyFactory instance = null;
+        private static readonly object lockObj = new object();
+
+        //variable 
+        List<EnemySoldier> EnemySoldiers = new List<EnemySoldier>();//יצירת הליסט
+        private static int NumberStaticOfCreate = 0;
+
+        //Constractor Private
+        private EnemyFactory() { }
+            
+        
+        //GetInstance//
+        public static EnemyFactory GetInstance()
+        {
+            //lock (lockObj)
+            //{
+                if (instance == null)
+                {
+                    instance = new EnemyFactory();
+
+                }
+            //}
+            return instance;
+        }
+        public void BuildingList()
+        {
+            EnemyFactory.NumberStaticOfCreate++;
+            //בשלב הזה אם צריך - לעשות swich
+            string nameObjectCreate = "enemySoldier" + NumberStaticOfCreate;
+            EnemySoldiers.Add(new EnemySoldier(nameObjectCreate));
+        }
+
+        // אופצייה של הוספת קוד דרך משתנים
+
+        //private static async Task startCode()
+        //{
+        //    ////לפני הרצה - חובה התקנה של 
+        //    ////Microsoft.CodeAnalysis.CSharp.Scripting
+
+        //    //string code = "Console.WriteLine(\"שלום מהקוד הדינמי!\");";
+        //    //await  CSharpScript.EvaluateAsync(code);
+        //}
+    }
+}
---
+++ Enemies/EnemySoldier.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using KomandoOOP;
+using KomandoOOP.Commands;
+using KomandoOOP.GameLogic;
+
+
+
+namespace KomandoOOP.Enemies
+{
+    internal class EnemySoldier
+    {
+        //API
+        /// <summary>
+        /// 
+        /// </summary>
+        /// 
+        
+        //filed
+        private string Name;
+        private int Life = 100;
+        public bool LifeStatus = true; //חי - 1 מת -0
+        
+        //constractor
+        public EnemySoldier(string name)
+        {
+            if(name == null)
+            {
+                Name = "";
+            }
+            else
+            {
+                Name = name;
+            }
+                
+        }
+        //Mehods
+        public void Shout()//צעקה
+        {
+            Console.WriteLine("i'm Enemy ! ");
+        }
+
+        public void LifeDownload()//הורד חיים
+        {
+            if (Life > 0)
+            {
+                Life--;
+            }
+            else
+            {
+                LifeStatus = false;
+            }
+        }
+
+
+    }
+}
