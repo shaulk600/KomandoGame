9211954be25e5e6deddeb7cb0a71ecc2576b85a8
Shaul Kimel <shaullk600@gmail.com>
2025-05-28 17:10:34 +0300

    Posh To Folder Weapones

---
+++ Weapons/WeaponFactory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomandoOOP.Weapons
+{
+    internal class WeaponFactory : IBreakable , IShootable
+    {
+        /// <summary>
+     ///  this Class is Factory
+     /// </summary>
+
+        // variable Singletone
+        private static EnemyFactory instance = null;
+        
+
+        //variable 
+        List<Weapon> Weapons = new List<Weapon>();//יצירת הליסט
+        private static int NumberStaticOfCreate = 0;
+
+        //Constractor Private
+        private WeaponFactory() { }
+
+
+        //GetInstance//
+        public static EnemyFactory GetInstance()
+        {
+            
+            if (instance == null)
+            {
+                instance = new WeaponFactory();
+
+            }
+            
+            return instance;
+        }
+        public void BuildingList(string name, string manufacturer, int numberOfBalls)
+        {
+            WeaponFactory.NumberStaticOfCreate++;
+            //בשלב הזה אם צריך - לעשות swich
+            switch (name)
+            {
+                case "AK-47":
+                    break;
+                case "M-16":
+                    break;
+                default:
+                    break;
+            }
+            string nameObjectCreate = "Weapon " + NumberStaticOfCreate;
+            Weapons.Add(new Weapon(name, manufacturer, numberOfBalls));
+        }
+        public static void GetInterFace()
+        {
+
+        }
+    }
+}
