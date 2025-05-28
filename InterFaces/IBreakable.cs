da496d9e6345e7ed34d7ab3ef13fdb8a8b15af9e
Shaul Kimel <shaullk600@gmail.com>
2025-05-28 17:18:23 +0300

    Posh To Folder InterFaces

---
+++ InterFaces/IBreakable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomandoOOP.InterFaces
+{
+    internal interface IBreakable // שביר
+    {
+        string ShowStatus {  get; } // הצג את הסטטוס
+        int NumberWeaponMax();  //   כמה כמות מכות מקסימלית
+        string ShowNumberStrokes();  // הצג כמות מכות נוכחית
+    }
+}
---
+++ InterFaces/IShootable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomandoOOP.InterFaces
+{
+    internal interface IShootable
+    {
+        void Shoot();//תירה 
+        int ShowNumberBalls(); // החזר כמות כדורים
+    }
+}
